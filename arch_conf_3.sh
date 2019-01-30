59c08e392b84fda8d0c24ef212d5f71343b639bc
Hoasker <hoasker@mail.ru>
2019-01-30 16:45:06 +0600

    Update arch_conf_3.sh

--- arch_conf_3.sh
+++ arch_conf_3.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
+
+# Установка "yay" из AUR
+# echo 'Установка YAY (yaourt)'
+#sudo pacman -Syu
+#mkdir -p /tmp/yay_install
+#cd /tmp/yay_install
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#makepkg -sir --needed --noconfirm --skippgpcheck
+#rm -rf yay_install
+
+# Установка pamac (установщик пакетов из AUR c GUI)
+#echo 'Установка PAC (pamac)'
+#sudo pacman -Syu
+#mkdir -p /tmp/pamac-aur_install
+#cd /tmp/pamac-aur_install
+#git clone https://aur.archlinux.org/pamac-aur.git
+#cd pamac-aur
+#makepkg -si --needed --noconfirm --skippgpcheck
+#rm -rf pamac-aur_install
+
 echo 'Установка AUR (aurman)'
 sudo pacman -Syy
 sudo pacman -S git --noconfirm
 
-#Ставим зависимость expac-git
+# Ставим зависимость expac-git
 git clone https://aur.archlinux.org/expac-git.git
 cd expac-git
 makepkg -si --noconfirm
 cd ..
 rm -rf expac-git
 
-#Ставим aurman
+# Ставим aurman
 git clone https://aur.archlinux.org/aurman.git
 cd aurman
 makepkg -si --noconfirm --skippgpcheck
